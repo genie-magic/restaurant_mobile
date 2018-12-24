565bb9f26e182613edc907285607c21f802da606
genie-magic <flashingtinker@gmail.com>
2018-12-25 04:02:56 +0800

    add fetch to "/cities" api and lazy load

--- lib/scenes/city.dart
+++ lib/scenes/city.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:restaurant_manage/common/meDrawer.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class CityScreen extends StatefulWidget {
   @override
@@ -7,6 +10,43 @@ class CityScreen extends StatefulWidget {
 }
 
 class CityScreenState extends State<CityScreen> {
+  final String url = 'http://localhost:8000/api/cities';
+
+  List<City> cityItems;
+
+  Future<City> getCityData() async {
+    var res = await http.get(Uri.encodeFull(url), headers: {"Accept": "application/json"});
+
+    setState((){
+      var response = json.decode(res.body);
+      var itemList = response['data'].map((val) => City.fromJsonMap(val)).toList();
+      cityItems = List<City>.from(itemList);
+    });
+  }
+
+  Widget _makeElement(int index) {
+    if (index >= cityItems.length) {
+      return null;
+    }
+
+    // print(cityItems[index].image_url);
+    print('http://localhost:8000${cityItems[index].image_url}');
+    return Container(
+      padding: EdgeInsets.all(5.0),
+      child: Column(
+        children: <Widget>[
+          Image.network('http://localhost:8000${cityItems[index].image_url}'),
+          Text(cityItems[index].name)
+        ],
+      )
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    this.getCityData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,12 +55,22 @@ class CityScreenState extends State<CityScreen> {
       appBar: AppBar (
         title: new Text("Navigation Drawer example")
       ),
-      body: Container(
-        child: Center(
-          child: Text("Home screen"),
+      body: Center(
+        child: ListView.builder(
+          itemCount: cityItems == null ? 0 : cityItems.length,
+          itemBuilder: (BuildContext context, int index) => _makeElement(index),
         )
       ),
       endDrawer: MeDrawer(),
     );
   }
+}
+
+class City {
+  final String name;
+  final String image_url;
+
+  City.fromJsonMap(Map map)
+    : name = map['name'],
+      image_url = map['image_url'];
 }
